85e9cdc5005ccf22cab24ca0414f236f632096fd
danielbrunobz <danielbrunobz@gmail.com>
2023-01-29 17:36:04 -0300

    correção de biblioteca

--- lib/app/core/rest_client/custom_dio.dart
+++ lib/app/core/rest_client/custom_dio.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
 
 import '../config/env/env.dart';
+
 
 class CustomDio extends DioForNative {
 
@@ -9,11 +11,18 @@ class CustomDio extends DioForNative {
     baseUrl: Env.i['backend_base_url'] ?? '',
     connectTimeout: 5000,
     receiveTimeout: 60000,)) {
-    nterceptors.add(LogInterceptor(
+    interceptors.add(LogInterceptor(
         requestBody: true,
         responseBody: true,
         requestHeader: true,
     ));
+  }
+
+  CustomDio auth(){
+    return this;
+  }
+  CustomDio unauth(){
+    return this;
   }
 
 
--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:dw9_delivery_app/app/core/config/env/env.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -15,9 +16,26 @@ class SplashPage extends StatelessWidget {
         children: [
           Container(),
           DeliveryButton(
+            width: 100,
+            heigth: 100,
             label: Env.i['backend_base_url'] ?? '',
             onPressed: (){},
           ),
+          Text(MediaQuery.of(context).size.width.toString()),
+          Text(context.screenWidth.toString()),
+          Row(
+            children: [
+              Container(color: Colors.red,
+              width: context.percentWidth(.5),
+                height: 100,
+              ),
+              Container(color: Colors.blue,
+                width: context.percentWidth(.5),
+                height: 100,
+              ),
+            ],
+          ),
+
           TextFormField(
             decoration: InputDecoration(labelText: 'text'),
           ),
