b227ed201fc66f2ec67eeca23811d48efd059279
danielbrunobz <danielbrunobz@gmail.com>
2023-02-10 20:33:50 -0300

    Implementações

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -3,7 +3,6 @@ import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Dw9DeliveryApp extends StatelessWidget {
   const Dw9DeliveryApp({Key? key}) : super(key: key);
--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,12 +1,11 @@
-import 'package:dw9_delivery_app/app/core/ui/helpers/loader.dart';
-import 'package:dw9_delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
-import 'package:dw9_delivery_app/app/models/product_model.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_controller.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_state.dart';
 import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/ui/base_state/base_state.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,13 +14,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Loader, Messages {
+class _HomePageState extends BaseState<HomePage, HomeController> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      context.read<HomeController>().loadProducts();
-    });
+  void onReady() {
+    controller.loadProducts();
   }
 
   @override
@@ -29,7 +25,20 @@ class _HomePageState extends State<HomePage> with Loader, Messages {
     return Scaffold(
       appBar: DeliveryAppbar(),
       body: BlocConsumer<HomeController, HomeState>(
-        listener: (context, state) {},
+        listener: (context, state) {
+          state.status.matchAny(
+              any: () => hideLoader(),
+              loading: () => showLoader(),
+              error: () {
+                hideLoader();
+                showError(state.errorMessage ?? 'Erro não informado');
+              });
+        },
+        buildWhen: (previous, current) => current.status.matchAny(
+          any: () => false,
+          initial: () => true,
+          loaded: () => true,
+        ),
         builder: (context, state) {
           return Column(
             children: [
